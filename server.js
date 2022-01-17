62aed28e610858ffe2f63ad4107daffde4c91a65
906284 <michaelcounseller@gmail.com>
2022-01-17 14:04:02 -0600

    parsed the db.json into json, and getting at /api/notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const data = require('./db/db.json')
 
 const path = require('path')
 
@@ -9,6 +10,11 @@ app.use(express.static('public'))
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+//Getting the data list and then parsing it into json
+app.get('/api/notes', (req, res) => {
+    res.json(data)
 })
 
 app.listen(PORT, () => {
