e969c5ab998607ae1bfc0c61662410eb1400076b
906284 <michaelcounseller@gmail.com>
2022-01-17 13:42:26 -0600

    adding a get so the user can view the notes.html when clicking get started

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+//Necessary code lines to get express.js started
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
 
 app.listen(PORT, () => {
     console.info(`The server connected to port ${PORT}`);
